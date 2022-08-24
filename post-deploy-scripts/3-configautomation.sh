abe7c22bc9b7e02d8edef6ef8fdc56452317aebb
Russell Hamker <russell@guarddogsolutions.com>
2022-08-24 15:36:43 -0600

    fix spelling

--- post-deploy-scripts/3-configautomation.sh
+++ post-deploy-scripts/3-configautomation.sh
@@ -20,7 +20,7 @@ EOL
 chmod +x /etc/cron.hourly/ospatch.sh > /dev/null 2>&1
 
 #Create Crontab scheduled event to run script
-echo "   Creating Crontab schedueled event for OS Patching ..."
+echo "   Creating Crontab scheduled event for OS Patching ..."
 sudo crontab -l > cron_bkp
 sudo echo "0 * * * * sudo /etc/cron.hourly/ospatch.sh >/dev/null 2>&1" >> cron_bkp
 sudo crontab cron_bkp
