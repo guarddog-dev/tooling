32ea96a0d7f7f2464203ca5a7515399b97277474
Russell Hamker <russell@guarddogsolutions.com>
2022-08-24 15:35:58 -0600

    update code

--- automation/Deploy-Virtual-Fido.sh
+++ automation/Deploy-Virtual-Fido.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 ##Install Extra Tools
-echo "  Deploying Virtual Fido ..."
+echo "Deploying Virtual Fido ..."
 
 #Provision System
-echo "   Provisioning System ..."
+echo "  Provisioning System ..."
 . /root/automation/1-run_vm_fido.sh.x
 
 
