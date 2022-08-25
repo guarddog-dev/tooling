4b67a84744fb84efd3a200077f6a4e2b641144d4
Russell Hamker <russell@guarddogsolutions.com>
2022-08-25 08:06:44 -0600

    fix for provisioning file

--- automation/Deploy-Virtual-Fido.sh
+++ automation/Deploy-Virtual-Fido.sh
@@ -2,6 +2,11 @@
 
 #Provision System
 echo "  Provisioning System ..."
+#create dist folder
+mkdir /root/automation/dist
+#move provisioning file to dist folder
+mv /root/automation/provisioning /root/automation/dist/provisioning
+#execute run fido automation
 . /root/automation/1-run_vm_fido.sh.x
 
 
