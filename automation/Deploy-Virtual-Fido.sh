0632ade0c63b5a52bf5cd2c390f6765edb438f70
Russell Hamker <russell@guarddogsolutions.com>
2022-10-06 11:44:39 -0600

    move deployment to kubernetes

--- automation/Deploy-Virtual-Fido.sh
+++ automation/Deploy-Virtual-Fido.sh
@@ -5,6 +5,10 @@ echo -e "\e[92m   Provisioning System ..." > /dev/console
 
 #execute run fido automation
 cd /root/automation
-./1-run_vm_fido.sh > /dev/console
 
+#Docker Method - Depricated
+#./1-run_vm_fido.sh > /dev/console 
+
+#Build Kubernetes Cluster and deploy vFido
+./build-virtual-fido.sh > /dev/console
 
